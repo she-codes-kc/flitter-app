89db600fbb735d7a357df1adf523973ad1528bf4
Sabrina Algaze <Sabri@MacBook-Pro-de-Sabrina.local>
2023-02-04 13:14:05 +0100

    connect to mongoose

---
+++ backend/lib/connectMongoose.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.connection.on('error', err => {
+    console.log('MongoDB connection failed', err);
+    process.exit(1);
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB at', mongoose.connection.name);
+});
+
+mongoose.connect('mongodb://127.0.0.1/flitter');
+
+module.exports = mongoose.connection;
+
+
