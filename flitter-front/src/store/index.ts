ca930e4bc3be2664457b10ccbc1520c4910e8b44
Anahi Garcia <gsanahi@gmail.com>
2023-02-13 07:37:06 +0100

    index store

--- flitter-front/src/store/index.ts
+++ flitter-front/src/store/index.ts
@@ -1,9 +1,8 @@
 import { createStore } from "vuex";
+import { user } from "@/store/users";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    user,
+  },
 });
