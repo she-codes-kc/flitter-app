da7333102867bffe0ce07e36742c60ba7d971390
Anahi Garcia <gsanahi@gmail.com>
2023-02-14 20:40:47 +0100

    funcionalidad del registro y validaciones en el back

--- backend/apps/auth/controllers/createUser.js
+++ backend/apps/auth/controllers/createUser.js
@@ -3,6 +3,15 @@
 const User = require('../../../models/User');
 
 const createUser = async (userData) => {
+    const {username, email} = userData;
+
+    if (await User.exists({username})) {
+        throw new Error("El usuario ya existe");
+    }
+    if (await User.exists({email})) {
+        throw new Error("El email ya existe");
+    }
+
     const user= new User(userData);
 
     return await user.save();
